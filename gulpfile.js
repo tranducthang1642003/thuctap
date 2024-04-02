fbc761c645a508d83f9a81222a1f8c93a6f6cee8
trần đức thắng <tranducthang.uway.@gmail.com>
2024-04-02 14:47:20 +0700

    cap nhat

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const{src,dest,watch,series} = require('gulp')
 const sass = require('gulp-sass') (require ('sass'))
 
 function buildStyles(){
-    return src('index.scss')
+    return src('*.index.scss')
        .pipe(sass())
        .pipe(dest('css'))
 }
  function watchTack(){
-     watch('index.scss',series(buildStyles))
+     watch('*.index.scss',series(buildStyles))
  }
  exports.default = series(buildStyles, watchTack)
